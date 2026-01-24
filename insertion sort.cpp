ed3e58f61d56708c4a63e16f806b8ba91338b94d
Omganesh Matiwade <omganeshmatiwade007@gmail.com>
2026-01-24 18:28:43 +0530

    Refactor insertion sort inner loop structure
    Refactor insertion sort to use a for loop for index j.

--- insertion sort.cpp
+++ insertion sort.cpp
@@ -9,15 +9,18 @@ int main() {
         cin >> a[i];
 
     for(int i = 1; i < n; i++) {
-        int key = a[i], j = i - 1;
-        while(j >= 0 && a[j] > key) {
+        int key = a[i];
+        int j;
+
+        for(j = i - 1; j >= 0 && a[j] > key; j--) {
             a[j + 1] = a[j];
-            j--;
         }
+
         a[j + 1] = key;
     }
 
     for(int i = 0; i < n; i++)
         cout << a[i] << " ";
+
     return 0;
 }
